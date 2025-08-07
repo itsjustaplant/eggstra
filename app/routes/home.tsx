8dafe0b65585ec91ce11df727321a61bec32b8b4
plant <alperensahin442@gmail.com>
2025-08-07 21:12:08 +0300

    feat :: add missing file

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,7 +5,7 @@ import { AnalysisCard } from "~/containers";
 import { TEXTS } from "~/texts";
 import type { TDailyData } from "~/types";
 import { EAnalysisType } from "~/types";
-import { getColorForProtein, getColorForWater, getDate } from "~/utils";
+import { getColorForProtein, getColorForWater } from "~/utils";
 import type { Route } from "./+types/home";
 
 // biome-ignore lint/correctness/noEmptyPattern: <shut the fuck up>
@@ -27,39 +27,6 @@ export async function loader({ context }: Route.LoaderArgs) {
     return { results };
   } catch (e) {
     console.log(e);
-  }
-}
-
-export async function action({ request, context }: Route.ActionArgs) {
-  const date = getDate();
-  const formData = await request.formData();
-  const protein = Number(formData.get("protein"));
-  const water = Number(formData.get("water"));
-
-  try {
-    const { results } = await context.cloudflare.env.DB.prepare(
-      `SELECT * FROM DailyData where Date = ?`
-    )
-      .bind(date)
-      .all<TDailyData>();
-
-    if (results.length === 0) {
-      await context.cloudflare.env.DB.prepare(
-        `INSERT INTO DailyData(Date, protein, water) values(?, ?, ?)`
-      )
-        .bind(date, protein, water)
-        .all();
-    } else {
-      await context.cloudflare.env.DB.prepare(
-        `UPDATE DailyData SET protein = ?, water = ? WHERE Date = ?`
-      )
-        .bind(results[0].protein + protein, results[0].water + water, date)
-        .run();
-    }
-
-    return { status: 200 };
-  } catch (_) {
-    return { status: 400 };
   }
 }
 
